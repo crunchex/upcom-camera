e443e4b37f7a7f20d098280247854be253cc75e2
Mike Lewis <mike@updroid.com>
2015-07-22 10:14:48 -0700

    web: added entry point for web

---
+++ web/index.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+import 'camera.dart';
+
+void main() {
+  ScriptElement jsmpgJs = new ScriptElement()
+    ..type = 'text/javascript'
+    ..src = 'http://localhost:12060/tabs/upcom-camera/jsmpg.js';
+  document.body.children.add(jsmpgJs);
+
+  jsmpgJs.onLoad.first.then((_) {
+    new UpDroidCamera(1, 1);
+  });
+}
